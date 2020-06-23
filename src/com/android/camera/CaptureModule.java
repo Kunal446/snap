5f4670d01af61b99f7aaed4ee26152899deca0b9
xianming wang <mingwax@codeaurora.org>
2020-06-23 18:54:08 +0800

    Snapcam: Fix GtsAssistantHostTestCases version 8.0_r1 case
    (1) Add respond front camera action for GTS case.
    (2) Add respond the right camera id for GTS case.
    
    Change-Id: Ifa2f08107aa6c89f10de78304c947c3a55b15591

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -3432,8 +3432,8 @@ public class CaptureModule implements CameraModule, PhotoController,
         int facingOfIntentExtras = CameraUtil.getFacingOfIntentExtras(mActivity);
         Log.v(TAG, "onResumeBeforeSuper facingOfIntentExtras :" + facingOfIntentExtras);
         if (facingOfIntentExtras != -1) {
-            mSettingsManager.setValue(SettingsManager.KEY_CAMERA_ID,
-                    String.valueOf(facingOfIntentExtras));
+            mSettingsManager.setValue(SettingsManager.KEY_SWITCH_CAMERA,
+                    facingOfIntentExtras == CameraUtil.FACING_BACK ? "rear" : "front");
         }
         mPaused = false;
         for (int i = 0; i < MAX_NUM_CAM; i++) {
