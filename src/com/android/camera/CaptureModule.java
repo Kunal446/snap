Vishalcj17/ParanoidCamera

Snapcam: Fix resume camera
When relaunch camera app, if camera is
closed, need to open camera again to
avoid black preview.

Change-Id: I800f0d2f246c1e4367671946b164b307e8f9eabd
CRs-Fixed: 2685731

src/com/android/camera/CaptureModule.java
@@ -1312,6 +1312,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             mCameraDevice[id] = null;
             mCameraOpenCloseLock.release();
             mCamerasOpened = false;
+            mIsCloseCamera = true;
         }
 
         @Override
@@ -1337,6 +1338,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             mCameraDevice[id] = null;
             mCameraOpenCloseLock.release();
             mCamerasOpened = false;
+            mIsCloseCamera = true;
         }
 
     };
@@ -9659,8 +9661,8 @@ public class CaptureModule implements CameraModule, PhotoController,
     public void restartAll() {
         setCurrentSceneModeOnly(mNextModeIndex);
         Log.d(TAG, "restart all: " + mOringalCameraId + "?=" + CURRENT_ID + "," + mNextModeIndex);
-        if(mOringalCameraId == CURRENT_ID){
-            //mIsCloseCamera = false;
+        if(mOringalCameraId == CURRENT_ID && mCameraDevice[mOringalCameraId] != null){
+            mIsCloseCamera = false;
         }else{
             mIsCloseCamera = true;
         }

Kunal446/snap

src/com/android/camera/CaptureModule.java
@@ -1343,6 +1343,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             mCameraDevice[id] = null;
             mCameraOpenCloseLock.release();
             mCamerasOpened = false;
+            mIsCloseCamera = true;
         }
 
         @Override
@@ -1366,6 +1367,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             mCameraDevice[id] = null;
             mCameraOpenCloseLock.release();
             mCamerasOpened = false;
+            mIsCloseCamera = true;
         }
 
     };
@@ -8515,7 +8517,7 @@ public class CaptureModule implements CameraModule, PhotoController,
         }
         int nextCameraId = getNextScreneModeId(mNextModeIndex);
         Log.d(TAG, "restart all CURRENT_ID :" + CURRENT_ID + " nextCameraId :" + nextCameraId);
-        if(CURRENT_ID == nextCameraId){
+        if(CURRENT_ID == nextCameraId && mCameraDevice[nextCameraId] != null){
             mIsCloseCamera = false;
         }else{
             mIsCloseCamera = true;