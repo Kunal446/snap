3235ee93284bc8e288fc741dafe4a11ab9fcde26
jinwu <jinwu@codeaurora.org>
2020-07-27 08:59:49 +0800

    Snapcam: Fix NPE of vendor tag
    Issues:Vendor tag will return null in snapcam reading them first time.
    Fixed: In order to no FC, catch NPE
    
    Change-Id: I8187bed2d9115c1624fb62243dd199538f7c28ae

--- src/com/android/camera/SettingsManager.java
+++ src/com/android/camera/SettingsManager.java
@@ -1281,7 +1281,7 @@ private void filterHFROptions() {
         try {
             byte bsgc_available = mCharacteristics.get(id).get(CaptureModule.bsgcAvailable);
             ret = bsgc_available == 1;
-        } catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException | NullPointerException e) {
             e.printStackTrace();
         }
         return ret;
@@ -1292,7 +1292,7 @@ private void filterHFROptions() {
         try {
             Byte logicalMode = mCharacteristics.get(id).get(CaptureModule.logicalMode);
             isLogicalCamera = logicalMode != null && logicalMode == (byte) 1;
-        } catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException | NullPointerException e) {
             e.printStackTrace();
         }
         return isLogicalCamera;
